dc56e9b8a75fddb9ff156d040f7f51d5dde7a0d7
Warda Nur <warduhh@gmail.com>
2023-03-07 04:30:00 +0000

    create new template file

--- express_server.js
+++ express_server.js
@@ -24,3 +24,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL};
+  res.render("urls_show", templateVars);
+});
