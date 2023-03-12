0bb7361882b768b290a0fceb477ed1199c5cbfc5
Warda Nur <warduhh@gmail.com>
2023-03-12 00:54:09 +0000

    Modified header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,73 +1,48 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+var cookieParser = require('cookie-parser')
 
 app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
+app.use(cookieParser())
 
 function generateRandomString() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
+ 
+  app.get('/urls', (req, res) => {
+    const templateVars = {
+      urls: urlDatabase,
+      username: req.cookies['username']
+    };
+    res.render('urls_index', templateVars);
+  });
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  app.post("/urls", (req, res) => {
+    const randomStr = generateRandomString();
+     urlDatabase[randomStr] = req.body.longURL; 
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+     app.get("/urls/new", (req, res) => {  
+      const templateVars = {
+        urls: urlDatabase,
+        username: req.cookies['username']
+      };
+        res.render("urls_new", templateVars);
+      });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+  app.post('/login', (req, res) => {
+    const username = req.body.email;
+    res.cookie('username', username);
+    res.redirect('/urls');
+  });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+  app.get('/login',(req, res) => {
+    res.render('urls_login')
+  });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+  app.get('/dashboard', (req, res) => {
+    const username = req.session.username;
+    res.render('dashboard', { username });
+  });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-// added short urls route 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL };
-  res.render("urls_show", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
-//"/u/:id" will redirect to its longURL
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  if (longURL)
-    res.redirect(longURL);
-});
-
-// This is route updates a URL resource; POST /urls/:id 
-app.post("/urls/:id", (req, res) => {   
-  const id = req.params.id;
-  const newUrl = req.body.longURL;
-  urlDatabase[id] = newUrl;
-  res.redirect("/urls");
-});
 
